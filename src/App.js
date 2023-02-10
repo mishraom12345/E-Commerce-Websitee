c825a67a5da3ade661265ab5399de9dbca96e62f
mishraom12345 <mishrahariom525@gmail.com>
2023-02-10 14:58:41 +0530

    rendering the items in list into the screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { productsArr } from './Component/Item/ProductList';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+const App =()=>{
+  const items = productsArr.map(item=>(
+    <li>
+      <h4>{item.title}</h4>
+      <img src = {item.imageUrl} />
+      <p>price: {item.price}</p>
+
+    </li>
+  ))
+  return(
+    <div >
+      <ul>{items}</ul>
     </div>
-  );
+  )
 }
 
 export default App;
