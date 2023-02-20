2d3d2d786221442bb24784b737302c797cc09587
mishraom12345 <mishrahariom525@gmail.com>
2023-02-20 20:28:24 +0530

    using lazy and suspenses

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { useState} from "react";
-import Header from "./components/Header";
-import CartList from "./components/CartList";
-import Home from "./components/pages/Home";
-import Store from "./components/pages/Store";
-import About from "./components/pages/About";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import ContactUs from "./components/pages/ContactUs";
-import SingleProduct from "./components/pages/ProductDetails";
-import Login from "./components/pages/Login";
-import ProductDetail from "./components/pages/ProductDetails";
+import { lazy,Suspense } from "react";
+import { useContext, useState } from "react";
+ import Header from "./components/Header";
+ import CartList from "./components/CartList";
+// import Home from "./components/pages/Home";
+// import Store from "./components/pages/Store";
+// import About from "./components/pages/About";
+ import "./App.css";
+ import { Routes, Route, Switch } from "react-router-dom";
+// import ContactUs from "./components/pages/ContactUs";
+
+ import Login from "./components/pages/Login";
+// import ProductDetail from "./components/pages/ProductDetails";
+ import AuthContext from "./components/store/AuthContext";
+ import Footer from "./components/Footer/Footer";
+
+const Home = lazy(()=>import("./components/pages/Home"))
+const Store = lazy(()=>import("./components/pages/Store"))
+const About = lazy(()=>import("./components/pages/About"))
+const ContactUs = lazy(()=>import("./components/pages/ContactUs"))
+const ProductDetail = lazy(()=>import( "./components/pages/ProductDetails"))
+//const Login = lazy(()=>import("./components/pages/Home"))
 
 const App = () => {
+  const authCtx = useContext(AuthContext)
   const [showCart, setShowCart] = useState(false);
 
   const handleShow = (value) => {
@@ -21,18 +32,19 @@ const App = () => {
   return (
     <div>
       <Header handleShow={handleShow} />
-      
 
-      <Routes>
-        
-        <Route path="/home" element={<Home />} />
-        <Route path="store" element={showCart ? <CartList /> : <Store />} />
-        <Route path="about" element={<About />} />
-        <Route path ='ContactUs' element={<ContactUs/>}/>
-        {<Route path='/product/:id' element={<ProductDetail/>}/>}
-        <Route path="login" element={<Login/>}/>
-        
-      </Routes>
+
+        <Routes>
+          <Route path="/home" element={<Suspense><Home /></Suspense>} />
+          <Route path="store" element={<Suspense>{showCart ? <CartList /> : <Store />}</Suspense>} />
+          <Route path="about" element={<Suspense><About /></Suspense>} />
+          <Route path="ContactUs" element={<Suspense><ContactUs /></Suspense>} />
+          {<Route path="/product/:id" element={<Suspense><ProductDetail /></Suspense>} />}
+          <Route path="/login" element={<Login />} />
+          
+        </Routes>
+        <Footer/>
+     
     </div>
   );
 };
