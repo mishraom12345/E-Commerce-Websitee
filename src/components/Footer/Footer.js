2ff373eafcc3e6801bd45c50e2bbcb358df16bf6
mishraom12345 <mishrahariom525@gmail.com>
2023-02-20 10:04:17 +0530

    Making footer for our website

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <div className="footer" >
+        <h2 className="heading">The Generics</h2>
+        <a href="https://www.facebook.com/" >
+          𝐟𝐚𝐜𝐞𝐛𝐨𝐨𝐤
+        </a>
+        <a href="https://www.youtube.com/">𝐲𝐨𝐮𝐭𝐮𝐛𝐞</a>
+        <a href="https://twitter.com/">𝙩𝙬𝙞𝙩𝙩𝙚𝙧</a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/pages/Store.js
+++ src/components/pages/Store.js
@@ -3,15 +3,17 @@ import React, { useContext } from "react";
 import { Cart } from "../CartContext";
 import "../../App.css";
 import { Link } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 const Store = () => {
   const { productsArr, addToCart } = useContext(Cart);
+  
 
   return (
     <div className="flex">
       {productsArr.map((item, index) => {
         return (
-          <div style={{ width: "50%" }}>
+          <div style={{ width: "50%",paddingBottom:'5%' }}>
             <div className="product-item">
               <h3 style={{ fontFamily: "Comic Sans MS" }}>{item.title}</h3>
               <Link to={`/product/${item.id}`}>
@@ -23,8 +25,10 @@ const Store = () => {
               <Button className="button" onClick={() => addToCart(item)}>
                 Add to Cart
               </Button>
+              
             </div>
           </div>
+          
         );
       })}
     </div>
